9855a5f8345a280201473aadf87f883b69061243
Sergen Bakır <cuseg09@gmail.com>
2022-09-10 11:34:29 +0300

    İsterler Tamamlandı

---
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/Frameworks/Commands/CreateFramework/CreateFrameworkCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.Frameworks.Commands.CreateFramework
+{
+    public class CreateFrameworkCommandValidator : AbstractValidator<CreateFrameworkCommand>
+    {
+        public CreateFrameworkCommandValidator()
+        {
+            RuleFor(x => x.Name).MinimumLength(2).NotEmpty(); ;
+            RuleFor(x => x.LanguageId).NotEmpty();
+            RuleFor(x => x.IsActive).NotEmpty();
+        }
+    }
+}
---
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/SocialMedias/Commands/CreateSocialMedia/CreateSocialMediaCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.SocialMedias.Commands.CreateSocialMedia
+{
+    public class CreateSocialMediaCommandValidator : AbstractValidator<CreateSocialMediaCommand>
+    {
+        public CreateSocialMediaCommandValidator()
+        {
+            RuleFor(x => x.GithubUrl).NotEmpty();
+            RuleFor(x => x.UserId).NotEmpty();
+        }
+    }
+}
