01163999d53dee51620aab7ce5afe3834a367a1b
Sergen Bakır <cuseg09@gmail.com>
2022-10-06 13:53:15 +0300

    UpdateUserOperationClaimCommand

---
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/UserOperationClaims/Commands/UpdateUserOperationClaim/UpdateUserOperationClaimCommand.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Core.Security.Entities;
+using Kodlama.io.Devs.Application.Features.UserOperationClaims.DTOs;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.UserOperationClaims.Commands.UpdateUserOperationClaim
+{
+    public class UpdateUserOperationClaimCommand : IRequest<UpdateUserOperationClaimDto>
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+        public bool IsActive { get; set; }
+
+        public class UpdateUserOperationClaimCommandHandler : IRequestHandler<UpdateUserOperationClaimCommand, UpdateUserOperationClaimDto>
+        {
+            #region Fields
+            private readonly IMapper _mapper;
+            private readonly IUserOperationClaimRepository _userOperationClaimRepository;
+            #endregion
+
+            #region Ctor
+            public UpdateUserOperationClaimCommandHandler(IMapper mapper,IUserOperationClaimRepository userOperationClaimRepository)
+            {
+                _mapper = mapper;
+                _userOperationClaimRepository = userOperationClaimRepository;
+            
+            }
+            #endregion
+
+            #region Handler
+            public async Task<UpdateUserOperationClaimDto> Handle(UpdateUserOperationClaimCommand request, CancellationToken cancellationToken)
+            {
+                UserOperationClaim mappedUserOperationClaim = 
+                    _mapper.Map<UserOperationClaim>(request);
+
+                UserOperationClaim updatedUserOperationClaim = 
+                    await _userOperationClaimRepository.UpdateAsync(mappedUserOperationClaim);
+
+                UpdateUserOperationClaimDto updateUserOperationClaimDto = 
+                    _mapper.Map<UpdateUserOperationClaimDto>(updatedUserOperationClaim);
+
+                return updateUserOperationClaimDto;
+            }
+            #endregion
+        }
+
+    }    
+}
--- src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/UserOperationClaims/Profiles/MappingProfiles.cs
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/UserOperationClaims/Profiles/MappingProfiles.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Core.Security.Entities;
 using Kodlama.io.Devs.Application.Features.UserOperationClaims.Commands.CreateUserOperationClaim;
+using Kodlama.io.Devs.Application.Features.UserOperationClaims.Commands.UpdateUserOperationClaim;
 using Kodlama.io.Devs.Application.Features.UserOperationClaims.DTOs;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,9 @@ namespace Kodlama.io.Devs.Application.Features.UserOperationClaims.Profiles
         {
             CreateMap<UserOperationClaim, CreateUserOperationClaimDto>().ReverseMap();
             CreateMap<UserOperationClaim, CreateUserOperationClaimCommand>().ReverseMap();
+
+            CreateMap<UserOperationClaim, UpdateUserOperationClaimDto>().ReverseMap();
+            CreateMap<UserOperationClaim, UpdateUserOperationClaimCommand>().ReverseMap();
         }
     }
 }
