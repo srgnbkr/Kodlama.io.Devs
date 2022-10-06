c7ffb37eb3ff1b076a87fd0856c926d0267c783f
Sergen Bakır <cuseg09@gmail.com>
2022-10-06 14:38:16 +0300

    CRUD Method Added To Controller

--- src/Kodlama.io.Devs/Kodlama.io.Devs.WebAPI/Controllers/UserOperationClaimsController.cs
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.WebAPI/Controllers/UserOperationClaimsController.cs
@@ -1,4 +1,8 @@
 ﻿using Core.Application.Requests;
+using Kodlama.io.Devs.Application.Features.UserOperationClaims.Commands.CreateUserOperationClaim;
+using Kodlama.io.Devs.Application.Features.UserOperationClaims.Commands.DeleteUserOperationClaim;
+using Kodlama.io.Devs.Application.Features.UserOperationClaims.Commands.UpdateUserOperationClaim;
+using Kodlama.io.Devs.Application.Features.UserOperationClaims.DTOs;
 using Kodlama.io.Devs.Application.Features.UserOperationClaims.Models;
 using Kodlama.io.Devs.Application.Features.UserOperationClaims.Queries.GetListUserOperationClaim;
 using Microsoft.AspNetCore.Http;
@@ -11,10 +15,31 @@ namespace Kodlama.io.Devs.WebAPI.Controllers
     public class UserOperationClaimsController : BaseController
     {
         [HttpGet("GetList")]
-        public async Task<IActionResult> GetList([FromQuery]PageRequest pageRequest)
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
         {
             GetListUserOperationClaimQuery getListUserOperationClaimQuery = new() { PageRequest = pageRequest };
             UserOperationClaimListModel result = await Mediator.Send(getListUserOperationClaimQuery);
+            return Ok(result);
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add([FromBody] CreateUserOperationClaimCommand createUserOperationClaimCommand)
+        {
+            CreateUserOperationClaimDto createUserOperationClaimDto = await Mediator.Send(createUserOperationClaimCommand);
+            return Created("", createUserOperationClaimDto);
+        }
+
+        [HttpPost("Update")]
+        public async Task<IActionResult> Update([FromBody] UpdateUserOperationClaimCommand updateUserOperationClaimCommand)
+        {
+            UpdateUserOperationClaimDto result = await Mediator.Send(updateUserOperationClaimCommand);
+            return Ok(result);
+        }
+
+        [HttpPost("Delete")]
+        public async Task<IActionResult> Delete([FromBody] DeleteUserOperaitonClaimCommand deleteUserOperaitonClaimCommand)
+        {
+            DeleteUserOperationClaimDto result = await Mediator.Send(deleteUserOperaitonClaimCommand);
             return Ok(result);
         }
     }
