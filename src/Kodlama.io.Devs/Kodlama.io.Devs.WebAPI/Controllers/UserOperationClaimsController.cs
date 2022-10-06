326f5acf4dbcbb2ed3853efc053ef1719d5a342f
Sergen Bakır <cuseg09@gmail.com>
2022-10-06 14:32:35 +0300

    UserOperationClaimController Added

---
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.WebAPI/Controllers/UserOperationClaimsController.cs
@@ -0,0 +1,21 @@
+﻿using Core.Application.Requests;
+using Kodlama.io.Devs.Application.Features.UserOperationClaims.Models;
+using Kodlama.io.Devs.Application.Features.UserOperationClaims.Queries.GetListUserOperationClaim;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kodlama.io.Devs.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserOperationClaimsController : BaseController
+    {
+        [HttpGet("GetList")]
+        public async Task<IActionResult> GetList([FromQuery]PageRequest pageRequest)
+        {
+            GetListUserOperationClaimQuery getListUserOperationClaimQuery = new() { PageRequest = pageRequest };
+            UserOperationClaimListModel result = await Mediator.Send(getListUserOperationClaimQuery);
+            return Ok(result);
+        }
+    }
+}
