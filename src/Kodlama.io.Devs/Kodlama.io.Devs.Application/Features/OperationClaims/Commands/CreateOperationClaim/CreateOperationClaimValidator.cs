f8e9891fe6fd05ce892e245b7af240dd1a6c2c5b
Sergen Bakır <cuseg09@gmail.com>
2022-10-10 16:15:45 +0300

    SecuredRequest Added

---
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/OperationClaims/Commands/CreateOperationClaim/CreateOperationClaimValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.OperationClaims.Commands.CreateOperationClaim
+{
+    public class CreateOperationClaimValidator : AbstractValidator<CreateOperationClaimCommand>
+    {
+        public CreateOperationClaimValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(c => c.Name).MinimumLength(3).WithMessage("Name must be at least 3 characters");
+
+        }
+    }
+}
